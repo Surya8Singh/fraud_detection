b789d3d75d77182897a07ad7ef680f758bbd16da
Surya8Singh <suryapratap.singh08@gmail.com>
2023-11-26 11:26:25 +0530

    3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding all ML related code here")
+
+def model1():
+    print("adding code to model1")
+
+def model2():
+    print("adding code to model2")
+
+def model3():
+    print("adding code to model3")
