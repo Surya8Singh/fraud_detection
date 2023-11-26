32deb1cd087062561c153823dd63745541848951
Surya8Singh <suryapratap.singh08@gmail.com>
2023-11-26 10:38:51 +0530

    adding new sum function

--- data_processing.py
+++ data_processing.py
@@ -2,3 +2,9 @@
 print("addting all the data processing code related to fraud detection")
 
 print("https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html")
+
+def sum(a,b):
+    c = a+b
+    return c
+
+print("added a new function sum")
